f4bd4779947626a413349e9fa08ae2582ab74f21
Sarah Sooklall <sarah.sooklall@f1006-04-34124.bmcc.cuny.edu>
2018-02-20 17:48:17 -0500

    adding madlibs example from class

--- madlibs/app.js
+++ madlibs/app.js
@@ -1,27 +1,27 @@
 /*
-    html element
+	html element references
 */
 const submitButton = document.getElementById("submit");
-const exclamationInput = document.getElementId("exclamation");
-const adverbInput =document.getElementId("adverb");
-const nounInput = document.getElementId("noun");
-const adjectiveInput = document.getElementId("adjective");
+const exclamationInput = document.getElementById("exclamation");
+const adverbInput = document.getElementById("adverb");
+const nounInput = document.getElementById("noun");
+const adjectiveInput = document.getElementById("adjective");
 
 /*
-    listen for button press
+	listen for button press
 */
 submitButton.onclick = function() {
-    /*
-        get all of the words, generate our story
-    */  
-    const message = document.createElement("p");
-    message.textContent = exclamationInput.value
-                            + " he said "
-                            + adverbInput.value
-                            + " as he jumped into his convertible "
-                            + nounInput.value
-                            + " and drove off with his "
-                            + adjectiveInput.value
-                            + " wife."
-    document.body.appendChild(message);
+	/*
+		get all of the words, generate our story
+	*/
+	const message = document.createElement("p");
+	message.textContent = exclamationInput.value
+							+ " he said "
+							+ adverbInput.value
+							+ " as he jumped into his convertible "
+							+ nounInput.value
+							+ " and drove off with his "
+							+ adjectiveInput.value
+							+ " wife."
+	document.body.appendChild(message);
 };
